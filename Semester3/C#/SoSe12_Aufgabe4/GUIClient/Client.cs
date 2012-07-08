5331da12008e598ecc3abf93e11d327ceb50b81a
Frank Denzer <fdenzer@gmail.com>
2012-07-08 17:12:30 +0200

    mv program -> client

--- Semester3/C#/SoSe12_Aufgabe4/GUIClient/Client.cs
+++ Semester3/C#/SoSe12_Aufgabe4/GUIClient/Client.cs
@@ -4,7 +4,7 @@ using System.Windows.Forms;
 
 namespace GUIClient
 {
-    static class Program
+    static class Client
     {
         /// <summary>
         /// The main entry point for the application.
